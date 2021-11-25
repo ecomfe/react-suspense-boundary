fd58125524c8b8d5037aeefbad39a00c82a77eeb
otakustay <otakustay@gmail.com>
2021-11-25 17:39:49 +0800

    fix: cache should return promise

---
+++ src/__tests__/hook.test.tsx
@@ -0,0 +1,26 @@
+import {ReactNode} from 'react';
+import {renderHook} from '@testing-library/react-hooks';
+import CacheProvider, {useConstantResource} from '../CacheProvider';
+
+interface ProviderProps {
+    children?: ReactNode;
+}
+
+function Provider({children}: ProviderProps) {
+    return (
+        <CacheProvider>
+            {children}
+        </CacheProvider>
+    );
+}
+
+const api = () => Promise.resolve(123);
+
+test('testable', async () => {
+    const {result, waitForNextUpdate} = renderHook(
+        () => useConstantResource(api),
+        {wrapper: Provider}
+    );
+    await waitForNextUpdate();
+    expect(result.current[0]).toBe(123);
+});
